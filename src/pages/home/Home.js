cef53f2b06942ad68296f9cc880dba5769d46ebb
Gstv <gstv.almeida09@gmail.com>
2021-12-05 22:16:07 -0300

    first commit

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Text, ScrollView } from "react-native";
+
+function Home() {
+  return (
+    <ScrollView>
+      <Text>Olá, mundo!!!</Text>
+    </ScrollView>
+  );
+}
+
+export default Home;
